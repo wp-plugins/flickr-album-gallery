8c4e161711c3335bb665e022e1b41cdfa7b5004b
weblizar <weblizar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-11-21 08:52:50 +0000

    New Version 1.3 - Russian Language Added
    git-svn-id: https://plugins.svn.wordpress.org/flickr-album-gallery/tags/1.3@1291381 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ flicker-album-gallery-help.php
@@ -0,0 +1,28 @@
+<div class="wrap" id="weblizar_wrap" >		
+	<div id="content_wrap">			
+		<div class="weblizar-header" >
+			<h2><span>Get Flicker Album Gallery Pro</span></h2>			
+			<div class="weblizar-submenu-links" id="weblizar-submenu-links">
+				<ul>
+					<li class=""> <div class="dashicons dashicons-format-chat" > </div> <a href="http://wordpress.org/plugins/flickr-album-gallery/" target="_blank" title="Support Forum"><?php _e('Support Forum', FAG_TEXT_DOMAIN); ?></a></li>
+					<li class=""> <div class="dashicons dashicons-welcome-write-blog"></div> <a href="<?php echo FAG_PLUGIN_URL.'readme.txt'?>" target="_blank" title="Theme Changelog"><?php _e('View Changelog', FAG_TEXT_DOMAIN); ?></a></li>      
+				</ul>
+			</div>			
+		</div>		
+		<div id="content">
+			<div id="options_tabs" class="ui-tabs">
+				<ul class="options_tabs ui-tabs-nav" role="tablist" id="nav">					
+					<li class="active"><a href="#" id="ourproduct" style="background-color: #141414;color: #F8F3F3;" ><div class="dashicons dashicons-plus"></div>Upgrade To Pro</a></li>
+					<li><a href="#" id="general"><div class="dashicons dashicons-format-chat"></div><?php _e('Need Help', FAG_TEXT_DOMAIN);?></a></li>
+					<li><a href="#" id="premium-product"><div class="dashicons dashicons-format-chat"></div>Premium Product</a></li>
+				</ul>					
+				<?php require_once('help-body.php'); ?>
+			</div>		
+		</div>
+		<div class="weblizar-header" style="margin-top:0px; border-radius: 0px 0px 6px 6px;">			
+			<div class="weblizar-submenu-links" style="margin-top:15px;">				
+			</div><!-- weblizar-submenu-links -->
+		</div>
+		<div class="clear"></div>
+	</div>
+</div>
---
+++ help-body.php
@@ -0,0 +1,320 @@
+<!---------------- Flicker Album Gallery Pro Tab  --------------->
+<div class="block ui-tabs-panel" id="option-ourproduct" >
+	<div class="row-fluid pricing-table pricing-three-column">
+		<div class="plan-name centre"> 
+			<h2>Flicker Album Gallery Pro</h2>
+			<h6>Get The Pro Flicker Album Gallery Plugin With Many Awesome Features</h6>
+			<div class="plan-name" style="padding-bottom:40px; text-align: center;float:left;width:100%">
+				<div class="purchase_btn_div" style="margin-top:20px;text-align: center;">
+				  <a  style= "margin-right:10px;" href="http://demo.weblizar.com/flickr-album-gallery-pro/" target="_new" class="btn btn-primary btn-lg">View Live Demo</a>
+				  <a href="http://weblizar.com/plugins/flickr-album-gallery-pro/" target="_new" class="btn btn-danger btn-lg">Upgrade To Pro</a>
+				</div>
+			</div>
+			<hr>
+		
+			<div class="col-md-4  demoftr "> 
+				<h2>Nivo Light Box</h2>
+				<div class="img-wrapper">
+					<div class="enigma_home_portfolio_showcase">
+						<img class="enigma_img_responsive ftr_img"  src="http://weblizar.com/wp-content/themes/home-theme/images/lightbox/nivo.jpg">
+						<div class="enigma_home_portfolio_showcase_overlay">
+							<div class="enigma_home_portfolio_showcase_overlay_inner ">
+								<div class="enigma_home_portfolio_showcase_icons">
+									<a title="Link" href="http://demo.weblizar.com/flickr-album-gallery-pro/gallery-with-nivo-light-box-slider/" target="_blank">Live Demo</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		
+			<div class="col-md-4  demoftr "> 
+				<h2>Photo Box</h2>
+				<div class="img-wrapper">
+					<div class="enigma_home_portfolio_showcase">
+						<img class="enigma_img_responsive ftr_img"  src="http://weblizar.com/wp-content/themes/home-theme/images/lightbox/photobox.jpg">
+						<div class="enigma_home_portfolio_showcase_overlay">
+							<div class="enigma_home_portfolio_showcase_overlay_inner ">
+								<div class="enigma_home_portfolio_showcase_icons">
+									<a title="Link" href="http://demo.weblizar.com/flickr-album-gallery-pro/gallery-with-photo-box-slider/" target="_blank">Live Demo</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		
+			<div class="col-md-4  demoftr "> 
+				<h2>Pretty Photo</h2>
+				<div class="img-wrapper">
+					<div class="enigma_home_portfolio_showcase">
+						<img class="enigma_img_responsive ftr_img"  src="http://weblizar.com/wp-content/themes/home-theme/images/lightbox/prettyphoto.jpg">
+						<div class="enigma_home_portfolio_showcase_overlay">
+							<div class="enigma_home_portfolio_showcase_overlay_inner ">
+								<div class="enigma_home_portfolio_showcase_icons">
+									<a title="Link" href="http://demo.weblizar.com/flickr-album-gallery-pro/gallery-with-pretty-photo-box-slider/" target="_blank">Live Demo</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		
+			<div class="col-md-4  demoftr "> 
+				<h2>Window Box</h2>
+				<div class="img-wrapper">
+					<div class="enigma_home_portfolio_showcase">
+						<img class="enigma_img_responsive ftr_img"  src="http://weblizar.com/wp-content/themes/home-theme/images/lightbox/window.png">
+						<div class="enigma_home_portfolio_showcase_overlay">
+							<div class="enigma_home_portfolio_showcase_overlay_inner ">
+								<div class="enigma_home_portfolio_showcase_icons">
+									<a title="Link" href="http://demo.weblizar.com/flickr-album-gallery-pro/gallery-with-window-box-slider/" target="_blank">Live Demo</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		
+			<div class="col-md-4  demoftr "> 
+				<h2>Smooth Box</h2>
+				<div class="img-wrapper">
+					<div class="enigma_home_portfolio_showcase">
+						<img class="enigma_img_responsive ftr_img"  src="http://weblizar.com/wp-content/themes/home-theme/images/lightbox/smooth.png">
+						<div class="enigma_home_portfolio_showcase_overlay">
+							<div class="enigma_home_portfolio_showcase_overlay_inner ">
+								<div class="enigma_home_portfolio_showcase_icons">
+									<a title="Link" href="http://demo.weblizar.com/flickr-album-gallery-pro/gallery-with-smooth-box-slider/" target="_blank">Live Demo</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		
+			<div class="col-md-4  demoftr "> 
+				<h2>Swipe Box</h2>
+				<div class="img-wrapper">
+					<div class="enigma_home_portfolio_showcase">
+						<img class="enigma_img_responsive ftr_img"  src="http://weblizar.com/wp-content/themes/home-theme/images/lightbox/swipebox.jpg">
+						<div class="enigma_home_portfolio_showcase_overlay">
+							<div class="enigma_home_portfolio_showcase_overlay_inner ">
+								<div class="enigma_home_portfolio_showcase_icons">
+									<a title="Link" href="http://demo.weblizar.com/flickr-album-gallery-pro/gallery-with-swipe-box-slider/" target="_blank">Live Demo</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		
+			<div class="col-md-4  demoftr "> 
+				<h2>Ion Box</h2>
+				<div class="img-wrapper">
+					<div class="enigma_home_portfolio_showcase">
+						<img class="enigma_img_responsive ftr_img"  src="http://weblizar.com/wp-content/themes/home-theme/images/lightbox/ion.jpg">
+						<div class="enigma_home_portfolio_showcase_overlay">
+							<div class="enigma_home_portfolio_showcase_overlay_inner ">
+								<div class="enigma_home_portfolio_showcase_icons">
+									<a title="Link" href="http://demo.weblizar.com/flickr-album-gallery-pro/gallery-with-ion-box-slider/" target="_blank">Live Demo</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		
+			<div class="col-md-4  demoftr "> 
+				<h2>Fancy Box</h2>
+				<div class="img-wrapper">
+					<div class="enigma_home_portfolio_showcase">
+						<img class="enigma_img_responsive ftr_img"  src="http://weblizar.com/wp-content/themes/home-theme/images/lightbox/fancy.jpg">
+						<div class="enigma_home_portfolio_showcase_overlay">
+							<div class="enigma_home_portfolio_showcase_overlay_inner ">
+								<div class="enigma_home_portfolio_showcase_icons">
+									<a title="Link" href="http://demo.weblizar.com/flickr-album-gallery-pro/gallery-with-fancy-box-slider/" target="_blank">Live Demo</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	
+		<div class="row pricing-table pricing-three-column" style="margin-top: 10px;">
+			<div class="col-md-4">
+				<div class=" plan ap">
+					<div class="plan-name">
+						<h2>Flickr Album </h2>
+						<span>Gallery Feature Set</span>
+					</div>
+					<ul>
+						<li class="plan-feature">Light Box Sliders</li>
+						<li class="plan-feature">All Gallery Shortcode</li>
+						<li class="plan-feature">Hide/Show Gallery Title</li>
+						<li class="plan-feature">Image Hover Animation</li>
+						<li class="plan-feature">Gallery Layout</li>
+						<li class="plan-feature">Hover Color</li>
+						<li class="plan-feature">Hover Color Opacity</li>
+						<li class="plan-feature">Single Gallery Shortcode</li>
+						<li class="plan-feature">Shortcode Button On Post/Page</li>
+						<li class="plan-feature">Unique Settings For Each Gallery</li>
+						<li class="plan-feature">Number of Hover Animation</li>
+						<li class="plan-feature">Number of Hover Color</li>
+						<li class="plan-feature">Number of Light Box Slider</li>
+						<li class="plan-feature">Number of Gallery Layout</li>
+					</ul>
+				</div>
+			</div>
+			
+			<div class="col-md-4">
+				<div class=" plan cp">
+					<div class="plan-name">
+						<h2>Pro</h2>
+						<span>$15</span>
+					</div>
+					<ul>
+						<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+						<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+						<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+						<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+						<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+						<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+						<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+						<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+						<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+						<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+						<li class="plan-feature">8</li>
+						<li class="plan-feature">Unlimited</li>
+						<li class="plan-feature">8</li>
+						<li class="plan-feature">6</li>
+						<li class="plan-feature">
+							<a class="btn btn-primary btn-lg" href="http://demo.weblizar.com/flickr-album-gallery-pro/" target="_new" style="font-size: large; font-weight: bolder;"><i class="fa fa-check-circle"></i> Live Demo</a>
+							<a class="btn btn-danger btn-lg" href="http://weblizar.com/amember/signup/flickr-album-gallery" target="_new" style="font-size: large; font-weight: bolder;"><i class="fa fa-shopping-cart"></i> Buy Now</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			
+			<div class="col-md-4">
+				<div class=" plan bp">
+					<div class="plan-name">
+						<h2>Free</h2>
+						<span>$0</span>
+					</div>
+					<ul>
+						<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+						<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+						<li class="plan-feature"><i class="fa fa-check fa-1x"></i></li>
+						<li class="plan-feature"><i class="fa fa-times fa-1x"></i></li>
+						<li class="plan-feature"><i class="fa fa-times fa-1x"></i></li>
+						<li class="plan-feature"><i class="fa fa-times fa-1x"></i></li>
+						<li class="plan-feature"><i class="fa fa-times fa-1x"></i></li>
+						<li class="plan-feature"><i class="fa fa-times fa-1x"></i></li>
+						<li class="plan-feature"><i class="fa fa-times fa-1x"></i></li>
+						<li class="plan-feature"><i class="fa fa-times fa-1x"></i></li>
+						<li class="plan-feature"><i class="fa fa-times fa-1x"></i></li>
+						<li class="plan-feature"><i class="fa fa-times fa-1x"></i></li>
+						<li class="plan-feature">1</li>
+						<li class="plan-feature">1</li>					
+						<li class="plan-feature">
+							<a href="#" class="button button-primary button-hero" style="font-size: large; font-weight: bolder;"><i class="fa fa-thumbs-up"></i> Enjoy Plugin</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<style>
+				.pricing-table .plan ul li.plan-feature {
+					padding: 8px !important;
+				}
+				.ap .plan-name {
+					background-color: #1E8CBE !important;
+				}
+				.bp .plan-name {
+					background-color: #1E8CBE !important;
+				}
+				.cp .plan-name {
+					background-color: #1E8CBE !important;
+				}
+				li {
+					font-size: larger !important;
+				}
+
+				.row-fluid .span4 {
+					width: 30.624% !important;
+				}
+				
+				ul li img {
+					
+				}
+			</style>
+		</div>
+	</div>
+</div>
+
+<!---------------- Premium Product Tab  --------------->
+<div class="block ui-tabs-panel deactive" id="option-premium-product">	
+	<div class="row-fluid pricing-table pricing-three-column">
+		<div class="plan-name centre"> 
+			<a style="margin-bottom:10px;textt-align:center" target="_new" href="http://weblizar.com"><img  src="http://weblizar.com/wp-content/themes/home-theme/images/weblizar2.png" /></a>
+		</div>	
+		<div class="plan-name">
+			<h2>Weblizar Responsive WordPress Theme</h2>
+			<h6>Get The Premium, And Create your website Beautifully</h6>
+		</div>
+		<div>
+			<div class="col-lg-6">
+				<h2>Premium Themes</h2><hr>
+				<ol id="weblizar_product">
+					<li><a target="_new" href="http://weblizar.com/themes/enigma-premium/">Enigma</a></li>
+					<li><a target="_new" href="http://weblizar.com/themes/weblizar-premium-theme/">Weblizar</a></li>					
+					<li><a target="_new" href="http://weblizar.com/themes/guardian-premium-theme/">Guardian</a></li>
+					<li><a target="_new" href="http://weblizar.com/plugins/green-lantern-premium-theme/">Green Lantern</a></li>
+					<li><a target="_new" href="https://weblizar.com/themes/creative-premium-theme/">Creative</a></li>
+					<li><a target="_new" href="https://weblizar.com/themes/incredible-premium-theme/">Incredible</a></li>
+				</ol>
+			</div>
+			<div class="col-lg-6">
+				<h2>Premium Plugins</h2><hr>
+				<ol id="weblizar_product">
+					<li><a target="_new" href="http://weblizar.com/ultimate-responsive-image-slider-pro/">Ultimate Responsive Image Slider</a></li>
+					<li><a target="_new" href="https://weblizar.com/plugins/photo-video-link-gallery-pro/">Photo Video Link Gallery Pro</a></li>
+					<li><a target="_new" href="http://weblizar.com/plugins/responsive-photo-gallery-pro//">Responsive Photo Gallery</a></li>
+					<li><a target="_new" href="http://weblizar.com/lightbox-slider-pro/">Lightbox Slider</a></li>
+					<li><a target="_new" href="http://weblizar.com/plugins/flickr-album-gallery-pro/">Flickr Album Gallery</a></li>
+					<li><a target="_new" href="https://weblizar.com/plugins/instagram-gallery-pro/">Instagram Gallery Pro</a></li>
+					<li><a target="_new" href="https://weblizar.com/plugins/gallery-pro/">Gallery Pro</a></li>					
+				</ol>
+			</div>
+		</div>	
+	</div>											
+	<div class="plan-name centre"> 
+		<div class="purchase_btn_div">
+			<div class="section container" id="product_decs">
+				<h3 align="center">Note: More details to click on Weblizar Products site link are below given view site button.</h3>	
+			</div>
+			<a href="http://www.weblizar.com" target="_new" class="btn btn-primary btn-lg dmobtn">View Site</a>		
+		</div>
+	</div>
+</div>
+
+<!---------------- Need Help Tab  --------------->
+<div class="block ui-tabs-panel deactive" id="option-general">	
+	<div class="row">
+		<div class="col-md-10">
+			<div id="heading">
+				<h2><?php _e('Flickr Album Gallery','weblizar_fag');?></h2>
+			</div>	
+			<p>Flickr Album Gallery is an awesome gallery plugin to display your Flickr on your WordPress blog.</p>
+			<br>
+			<p>Flickr Album Gallery is based on Flickr JS API. Use plugin to display all your Flickr Albums on your WordPress Blog Site. Flickr Album Gallery is based on Flickr JS API. Use plugin to display all your Flickr Albums on your Wordpress Blog Site. You can use your own Flickr API and Flickr Album ID to publish Flickr Albums on your WordPress Blog website.</p>
+			<br>
+			<p>Flickr Album Gallery plugin is such a easy to use and fully documented ready plugin.</p>
+			<br>
+			<p>Using <strong>[FAG id=xx]</strong> shortcode, you can publish Flickr Album Gallery into any Page or Post.</p>
+			<br>
+			<h4>Read Flickr Album Gallery Full Documentation <a href="http://www.weblizar.com/flickr-album-gallery-plugin-documentation/" target="_blank">Here</a></h4>
+		</div>
+	</div>
+</div>
